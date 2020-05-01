10cec19ac0de46019eec3e1ed3f1ae63f6ffd8dd
Luís Martinho <lmartinho@gmail.com>
2020-05-01 23:03:41 +0100

    Brush up on basic R data structures

---
+++ data_types.R
@@ -0,0 +1,10 @@
+# Combine Values into a Vector or List
+c <- c(7, 6, 3, 8, 6, 8, 7, 4, 4, 5, 8, 4) #, 9, 2)
+# Converts to vector (looks the same)
+v <- as.vector(c)
+# Converts to data (now allow preview)
+l <- as.list(c)
+
+summary(v)
+# TODO: How can we summarize the elements in the lists
+summary(l)
